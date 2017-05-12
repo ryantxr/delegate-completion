2ac8950e140fb1dc5e095bd440117c3c4d5ebc3b
ryantxr <ryantxr@gmail.com>
2017-05-12 09:27:46 -0400

    Initial Commit

---
+++ getsum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getsum
+//
+//  Created by ryan teixeira on 5/12/17.
+//  Copyright © 2017 blazecore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
